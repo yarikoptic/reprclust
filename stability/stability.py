33dda77ea9ef80c5a44622e820127c2f05a0e3c9
Matteo Visconti dOC <matteo.visconti.gr@dartmouth.edu>
2015-06-01 11:45:28 -0400

    Fix docstring for stability, plus API break
    Log-likelihood is now returned as the last column, for
    consistency of the first three columns of result.

--- stability/stability.py
+++ stability/stability.py
@@ -1,15 +1,40 @@
+# -*- coding: utf-8 -*-
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+#
+#   See COPYING file distributed along with the module for the
+#   copyright and license terms.
+#
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+"""
+Module to compute stability estimates of clustering solutions using a method
+inspired by Yeo et al. (2011). At the moment the following clustering
+algorithms are implemented:
+    - kmeans
+    - Gaussian Mixture Models
+    - Ward Clustering (structured and unstructured)
+    - complete with correlation distance
+
+References:
+-----------
+Thomas Yeo, B. T., Krienen, F. M., Sepulcre, J., Sabuncu, M. R., Lashkari, D.,
+Hollinshead, M., et al. (2011). The organization of the human cerebral cortex
+estimated by intrinsic functional connectivity.
+Journal of Neurophysiology, 106(3), 1125–1165. doi:10.1152/jn.00338.2011
+"""
 import numpy as np
 
-from scipy.spatial.distance import (pdist, squareform, cdist)
-from scipy.cluster.hierarchy import complete, ward
+from scipy.spatial.distance import pdist
+from scipy.cluster.hierarchy import complete
 
-from sklearn.metrics.cluster import adjusted_rand_score, adjusted_mutual_info_score
+from sklearn.metrics.cluster import (adjusted_rand_score,
+                                     adjusted_mutual_info_score)
 from sklearn.cluster.hierarchical import _hc_cut, ward_tree
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.mixture import GMM
 from sklearn.cluster import KMeans
 
 from joblib import Parallel, delayed
+
 AVAILABLE_METHODS = ['ward', 'complete', 'gmm', 'kmeans']
 
 
@@ -22,28 +47,41 @@ def compute_stability_fold(samples, train, test, method='ward',
                            max_k=300, stack=False, cv_likelihood=False,
                            **kwargs):
     """
-    General function to compute the stability on a fold.
+    General function to compute the stability on a cross-validation fold.
     
     Parameters:
-        samples: a list of arrays containing the samples to cluster, each
-                 array has shape (n_samples, n_features) in PyMVPA terminology.
-                 We are clustering the features, i.e., the nodes.
-        train: a list of indices for the training set
-        test: a list of indices for the test set
-        method: method to use. atm only 'ward', 'complete', 'gmm',
-                and 'kmeans' are implemented.
-        max_k: maximum k to compute the stability testing.
-        stack: if False, datasets are averaged; if True, they are stacked.
-        cv_likelihood: compute the cross-validated likelihood for mixture model;
-                       only valid if 'gmm' method is used.
-        kwargs: optional keyword arguments being passed to the clustering method
-                (only for 'ward', and 'gmm')
+    -----------
+        samples : list of arrays
+            List of arrays containing the samples to cluster, each
+            array has shape (n_samples, n_features) in PyMVPA terminology.
+            We are clustering the features, i.e., the nodes.
+        train : list or array
+            Indices for the training set.
+        test : list or array
+            Indices for the test set.
+        method : {'complete', 'gmm', 'kmeans', 'ward'}
+            Clustering method to use. Default is 'ward'.
+        max_k : int
+            Maximum k to compute the stability testing, starting from 2.
+        stack : bool
+            Whether to stack or average the datasets. Default is False,
+            meaning that the datasets are averaged by default.
+        cv_likelihood : bool
+            Whether to compute the cross-validated likelihood for mixture
+            model; only valid if 'gmm' method is used. Default is False.
+        kwargs : optional
+            Keyword arguments being passed to the clustering method (only for
+            'ward' and 'gmm').
     
-    Attributes:
-        result: a (max_k-1, 3) array, where result[:, 0] is the ARI,
-                result[:, 1] is the AMI, and result[:, 2] is the k.
-                if method is 'gmm' and cv_likelihood is True, result[:, 2]
-                is the cross-validated likelihood, and k moves to result[:, 3]
+    Returns:
+    --------
+        result: array
+            A (max_k-1, 3) array, where result[:, 0] is the Adjusted Rand
+            Index, result[:, 1] is the Adjusted Mutual Information, and
+            result[:,  2] is the corresponding k.
+            If method is 'gmm' and cv_likelihood is True, it returns a
+            (max_k-1, 4) array, where result[:, 3] is the cross-validated
+            likelihood.
         
     """    
     if method not in AVAILABLE_METHODS:
@@ -74,7 +112,8 @@ def compute_stability_fold(samples, train, test, method='ward',
     if method == 'complete':
         train_ds_dist = pdist(train_ds.T, metric='correlation')
         test_ds_dist = pdist(test_ds.T, metric='correlation')
-        # I'm computing the full tree and then cutting afterwards to speed computation
+        # I'm computing the full tree and then cutting
+        # afterwards to speed computation
         Y_train = complete(train_ds_dist)
         # same on testing set
         Y_test = complete(test_ds_dist)
@@ -94,9 +133,7 @@ def compute_stability_fold(samples, train, test, method='ward',
             # cut the tree with right K for both train and test
             train_label = cut_tree_scipy(Y_train, k)
             test_label = cut_tree_scipy(Y_test, k)
-
             # train a classifier on this clustering
-            #knn = KNeighborsClassifier(metric=correlation)
             knn = KNeighborsClassifier(algorithm='brute', metric='correlation')
             knn.fit(train_ds.T, train_label)
             # predict the clusters in the test set
@@ -105,7 +142,6 @@ def compute_stability_fold(samples, train, test, method='ward',
             # cut the tree with right K for both train and test
             train_label = _hc_cut(k, children_train, n_leaves_train)
             test_label = _hc_cut(k, children_test, n_leaves_test)
-
             # train a classifier on this clustering
             knn = KNeighborsClassifier()
             knn.fit(train_ds.T, train_label)
@@ -126,7 +162,6 @@ def compute_stability_fold(samples, train, test, method='ward',
             # fit on train and predict test
             kmeans.fit(train_ds.T)
             prediction_label = kmeans.predict(test_ds.T)
-
             # fit on test and get labels
             kmeans.fit(test_ds.T)
             test_label = kmeans.predict(test_ds.T)
@@ -138,47 +173,63 @@ def compute_stability_fold(samples, train, test, method='ward',
         result[i_k, 1] = adjusted_mutual_info_score(prediction_label,
                                                     test_label)
         if cv_likelihood:
-            result[i_k, 2] = log_prob
+            result[i_k, -1] = log_prob
 
-    result[:, -1] = range(2, max_k+1)
-
+    result[:, 2] = range(2, max_k+1)
     return result
 
 
 def compute_stability(splitter, samples, method='ward', stack=False,
                       cv_likelihood=False, max_k=300, n_jobs=1, **kwargs):
     """
-    General function to compute the stability of clustering on a dataset.
-    
+    General function to compute the stability of clustering on a list of
+    datasets.
+
     Parameters:
-        splitter: a generator of training and test set, usually from sklearn.cross_validation
-        samples: a list of arrays containing the samples to cluster, each
-                 array has shape (n_samples, n_features) in PyMVPA terminology.
-                 We are clustering the features, i.e., the voxels/nodes.
-        method: method to use. atm only 'ward', 'complete', 'gmm',
-                and 'kmeans' are implemented.
-        stack: if False, datasets are averaged; if True, they are stacked.
-        cv_likelihood: compute the cross-validated likelihood for mixture model;
-                       only valid if 'gmm' method is used
-        max_k: maximum k to compute the stability testing.
-        n_jobs: number of jobs to run the parallelization. default n_jobs=1
-        kwargs: optional keyword arguments being passed to the clustering method
-                (only for 'ward', and 'gmm'). Useful to pass connectivity matrix (ward)
-                or covariance structure and random initial state (GMM).
-    
-    Attributes:
-        result: a (max_k-1, 3) array, where result[:, 0] is the ARI, 
-                result[:, 1] is the AMI, and result[:, 2] is the k;
-                if method is 'gmm' and cv_likelihood is True, result[:, 2]
-                is the cross-validated likelihood, and k moves to result[:, 3]
-        
-    """    
-    
+    -----------
+        splitter
+            A generator of training and test set, usually from
+            sklearn.cross_validation.
+        samples : list of arrays
+            List of arrays containing the samples to cluster, each
+            array has shape (n_samples, n_features) in PyMVPA terminology.
+            We are clustering the features, i.e., the nodes.
+        train : list or array
+            Indices for the training set.
+        test : list or array
+            Indices for the test set.
+        method : {'complete', 'gmm', 'kmeans', 'ward'}
+            Clustering method to use. Default is 'ward'.
+        max_k : int
+            Maximum k to compute the stability testing, starting from 2.
+        stack : bool
+            Whether to stack or average the datasets. Default is False,
+            meaning that the datasets are averaged by default.
+        cv_likelihood : bool
+            Whether to compute the cross-validated likelihood for mixture
+            model; only valid if 'gmm' method is used. Default is False.
+        n_jobs : int
+            Number of jobs (cores) to run the algorithm on. Default is 1.
+        kwargs : optional
+            Keyword arguments being passed to the clustering method (only for
+            'ward' and 'gmm').
+
+    Returns:
+    --------
+        result: array
+            A (max_k-1, 3) array, where result[:, 0] is the Adjusted Rand
+            Index, result[:, 1] is the Adjusted Mutual Information, and
+            result[:,  2] is the corresponding k.
+            If method is 'gmm' and cv_likelihood is True, it returns a
+            (max_k-1, 4) array, where result[:, 3] is the cross-validated
+            likelihood.
+
+    """
+
     result = Parallel(n_jobs=n_jobs)(delayed(compute_stability_fold)
                                      (samples, train, test, method=method,
-                                     max_k=max_k, stack=stack, cv_likelihood=cv_likelihood,
+                                     max_k=max_k, stack=stack,
+                                     cv_likelihood=cv_likelihood,
                                      **kwargs) for train, test in splitter)
-
     result = np.vstack(result)
-    
     return result
