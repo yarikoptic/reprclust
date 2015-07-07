9a3571c116219f24c66e5bc0a658d1aeedc34c16
Matteo Visconti dOC <matteo.visconti.gr@dartmouth.edu>
2015-07-07 15:24:50 -0400

    Refactor cluster metrics as scores and run tests

--- reprclust/cluster_metrics.py
+++ reprclust/cluster_metrics.py
@@ -17,11 +17,8 @@ from sklearn.metrics.cluster import (adjusted_rand_score,
                                      adjusted_mutual_info_score)
 from sklearn.utils.linear_assignment_ import linear_assignment
 
-# alias
-ari = adjusted_rand_score
-ami = adjusted_mutual_info_score
-
-def get_optimal_permutation(a, b, k):
+# functions
+def _get_optimal_permutation(a, b, k):
     """Finds an optimal permutation `p` of the elements in b to match a,
     using the Hungarian algorithm, such that `a ~ p(b)`. `k` specifies the
     number of possible labels, in case they're not all present in `a` or `b`.
@@ -40,7 +37,7 @@ def get_optimal_permutation(a, b, k):
     return permutation[:, 1]
 
 
-def permute(b, permutation):
+def _permute(b, permutation):
     """Permutes the element in b using the mapping defined in `permutation`.
     Value `i` in b is mapped to `permutation[i]`.
     """
@@ -50,105 +47,130 @@ def permute(b, permutation):
     return out
 
 
-def stability_score(predicted_label, test_label, k):
+class ClusterMetric(object):
+    def __init__(self, *args, **kwargs):
+        self._args = args
+        self._kwargs = kwargs
+
+    def __call__(self, test_label, predicted_label, data, *args, **kwargs):
+        raise NotImplementedError
+
+
+class ARI(ClusterMetric):
+    def __init__(self):
+        super(ARI, self).__init__()
+
+    def __call__(self, test_label, predicted_label, *args, **kwargs):
+        return adjusted_rand_score(test_label, predicted_label)
+
+
+class AMI(ClusterMetric):
+    def __init__(self):
+        super(AMI, self).__init__()
+
+    def __call__(self, test_label, predicted_label, *args, **kwargs):
+        return adjusted_mutual_info_score(test_label, predicted_label)
+
+
+class CorrelationScore(ClusterMetric):
+    def __init__(self, corr_type='pearson'):
+        super(CorrelationScore, self).__init__()
+        self._corr_type = corr_type
+
+    def __call__(self, test_label, predicted_label, data, *args, **kwargs):
+        return self._correlation_score(predicted_label, test_label, data,
+                                       corr_type=self._corr_type)
+
+    @staticmethod
+    def _correlation(x, y):
+        """Compute correlation of vectors x and y"""
+        c1 = x - x.mean()
+        c2 = y - y.mean()
+        return np.dot(c1, c2)/np.sqrt(((c1**2).sum() * (c2**2).sum()))
+
+    def _correlation_score(self, predicted_label, test_label, data,
+                           corr_type='pearson'):
+        """Computes the correlation between the average RDMs in each
+        corresponding cluster.
+        """
+        unique_test = np.unique(test_label)
+        unique_pred = np.unique(predicted_label)
+        # if we have completely different labels, complain
+        if not set(unique_test).intersection(unique_pred):
+            raise ValueError("predicted_label and test_label have completely "
+                         "different labellings, I don't know what to do with "
+                         "this.")
+        # get permutation to match predicted_label to test_label
+        k = max(len(np.unique(predicted_label)), len(np.unique(test_label)),
+                np.max(predicted_label) + 1, np.max(test_label) + 1)
+        perm = _get_optimal_permutation(test_label, predicted_label, k)
+        # permute
+        predicted_label = _permute(predicted_label, perm)
+        # compute correlation across corresponding clusters
+        corr = 0
+        # to account for the case in which I'm testing against ground truth,
+        # I need to cycle through the intersection of unique labels
+        # recompute the unique labels after permutation
+        unique_test = np.unique(test_label)
+        unique_pred = np.unique(predicted_label)
+        labels = np.intersect1d(unique_test, unique_pred, assume_unique=True)
+        for i in labels:
+            assert(len(np.unique(data[:, test_label == i])) > 0)
+            assert(len(np.unique(data[:, predicted_label == i])) > 0)
+            c1 = np.mean(data[:, test_label == i], axis=-1)
+            c2 = np.mean(data[:, predicted_label == i], axis=-1)
+            if corr_type == 'pearson':
+                corr += self._correlation(rankdata(c1), rankdata(c2))
+            elif corr_type == 'spearman':
+                corr += self._correlation(c1, c2)
+            else:
+                raise ValueError("We shouldn't get here.")
+        corr /= len(labels)
+
+        return corr
+
+
+class InstabilityScore(ClusterMetric):
+    def __init__(self, normalization_scores):
+        """Initialize with a dictionary of normalization scores"""
+        super(InstabilityScore, self).__init__()
+        self._normalization_scores = normalization_scores
+
+    def __call__(self, test_label, predicted_label, k, *args, **kwargs):
+        if k not in self._normalization_scores:
+            raise ValueError('Provided normalization scores do not have '
+                             'selected value of k {0}'.format(k))
+
+        return _instability_score(predicted_label, test_label,
+                                  k)/self._normalization_scores[k]
+
+
+def _instability_score(predicted_label, test_label, k):
     """Computes the stability score (see Lange) for `predicted_label` and
     `test_label` assuming `k` possible labels.
     """
     # find optimal permutation of labels between predicted and test
-    test_label_ = permute(test_label,
-                          get_optimal_permutation(predicted_label,
-                                                  test_label, k))
+    test_label_ = _permute(test_label,
+                           _get_optimal_permutation(predicted_label,
+                                                    test_label, k))
     # return hamming distance
     return hamming(predicted_label, test_label_)
 
 
-def generate_random_labeling(k, size):
-    """Generate a valid random labeling"""
-    if size < k:
-        raise ValueError('To have a labeling size cannot be lower than high')
-    while True:
-        a = np.random.randint(0, k, size)
-        if len(np.unique(a)) == k:
-            break
-    return a
-
-
-def rand_stability_score(k, n, s):
+def rand_instability_score(ks, n, s):
     """Generates a score for random k-labellings of length n based on s
     iterations. Used as denominator to normalize the stability score.
+    ks can be a numpy array of ks, and a dictionary is returned.
     """
     # get s random labellings and compute their score
-    rand_score = 0
-    for i in xrange(s):
-        # TODO: optimize this to generate fewer random labellings
-        #rand_label1 = generate_random_labeling(k, n)
-        #rand_label2 = generate_random_labeling(k, n)
-        rand_label1 = np.random.randint(0, k, n)
-        rand_label2 = np.random.randint(0, k, n)
-        rand_score += stability_score(rand_label1, rand_label2, k)
-    rand_score /= s
-    return rand_score
+    rand_scores = {}
 
-
-def norm_stability_score(predicted_label, test_label, rand_score, k):
-    """Computes the normalized stability score (see Lange) for
-    `predicted_label` and  `test_label`. The stability score is normalized
-    using a random labeling algorithm `R_k` (see original paper).
-    """
-    assert 0. <= rand_score <= 1., 'rand_score of {0} is not a valid ' \
-                                   'distance'.format(rand_score)
-    # compute score and normalize it
-    score = stability_score(predicted_label, test_label, k)/rand_score
-    return score
-
-
-def correlation(x, y):
-    """Compute correlation of vectors x and y"""
-    c1 = x - x.mean()
-    c2 = y - y.mean()
-    return np.dot(c1, c2)/np.sqrt(((c1**2).sum() * (c2**2).sum()))
-
-
-def correlation_score(predicted_label, test_label, data, corr_score='pearson'):
-    """Computes the correlation between the average RDMs in each
-    corresponding cluster.
-    """
-    unique_test = np.unique(test_label)
-    unique_pred = np.unique(predicted_label)
-    # if we have completely different labels, complain
-    if not set(unique_test).intersection(unique_pred):
-        raise ValueError("predicted_label and test_label have completely "
-                         "different labellings, I don't know what to do with "
-                         "this.")
-    # get permutation to match predicted_label to test_label
-    k = max(len(np.unique(predicted_label)), len(np.unique(test_label)),
-            np.max(predicted_label) + 1, np.max(test_label) + 1)
-    perm = get_optimal_permutation(test_label, predicted_label, k)
-    # permute
-    predicted_label = permute(predicted_label, perm)
-    # compute correlation across corresponding clusters
-    corr = 0
-    # to account for the case in which I'm testing against ground truth,
-    # I need to cycle through the intersection of unique labels
-    # recompute the unique labels after permutation
-    unique_test = np.unique(test_label)
-    unique_pred = np.unique(predicted_label)
-    #    if len(unique_test) <= len(unique_pred):
-    #        labels = unique_test
-    #    else:
-    #        labels = unique_pred
-    labels = np.intersect1d(unique_test, unique_pred, assume_unique=True)
-    for i in labels:
-        assert(len(np.unique(data[:, test_label == i])) > 0)
-        assert(len(np.unique(data[:, predicted_label == i])) > 0)
-        c1 = np.mean(data[:, test_label == i], axis=-1)
-        c2 = np.mean(data[:, predicted_label == i], axis=-1)
-        if corr_score == 'pearson':
-            corr += correlation(rankdata(c1), rankdata(c2))
-        elif corr_score == 'spearman':
-            corr += correlation(c1, c2)
-        else:
-            raise ValueError("We shouldn't get here.")
-    corr /= len(labels)
-
-    return corr
+    for k in ks:
+        rand_score = 0
+        for i in xrange(s):
+            rand_label1 = np.random.randint(0, k, n)
+            rand_label2 = np.random.randint(0, k, n)
+            rand_score += _instability_score(rand_label1, rand_label2, k)
+        rand_score /= s
+        rand_scores[k] = rand_score
+    return rand_scores
--- reprclust/tests/test_cluster_metrics.py
+++ reprclust/tests/test_cluster_metrics.py
@@ -5,13 +5,23 @@ from nose.tools import assert_equal, assert_true, assert_raises, \
 import numpy as np
 from numpy.testing import assert_array_equal
 
-from reprclust.cluster_metrics import (generate_random_labeling, permute,
-                                       get_optimal_permutation,
-                                       stability_score,
-                                       rand_stability_score,
-                                       norm_stability_score,
-                                       correlation_score,
-                                       correlation)
+from reprclust.cluster_metrics import (_permute,
+                                       _get_optimal_permutation,
+                                       _instability_score,
+                                       rand_instability_score,
+                                       AMI, ARI, CorrelationScore,
+                                       InstabilityScore)
+
+
+def generate_random_labeling(k, size):
+    """Generate a valid random labeling"""
+    if size < k:
+        raise ValueError('To have a labeling size cannot be lower than high')
+    while True:
+        a = np.random.randint(0, k, size)
+        if len(np.unique(a)) == k:
+            break
+    return a
 
 
 def test_generate_random_labeling():
@@ -23,12 +33,12 @@ def test_generate_random_labeling():
 def test_permute():
     a = np.arange(9)
     b = np.arange(9)[::-1]
-    assert_array_equal(a, permute(b, b))
+    assert_array_equal(a, _permute(b, b))
 
     a = np.array([0, 0, 1, 2, 1, 3])
     b = np.array([1, 1, 3, 2, 3, 0])
     p = np.array([3, 0, 2, 1])
-    assert_array_equal(a, permute(b, p))
+    assert_array_equal(a, _permute(b, p))
 
 
 def test_get_optimal_permutation():
@@ -36,9 +46,9 @@ def test_get_optimal_permutation():
     a = np.array([0, 1, 2, 3])
     b = np.array([3, 2, 1, 0])
     perm_real = b
-    assert_array_equal(perm_real, get_optimal_permutation(a, b, 4))
+    assert_array_equal(perm_real, _get_optimal_permutation(a, b, 4))
     # no permutation
-    assert_array_equal(a, get_optimal_permutation(a, a, 4))
+    assert_array_equal(a, _get_optimal_permutation(a, a, 4))
     # random labellings
     k = 5
     for i in xrange(20):
@@ -49,16 +59,16 @@ def test_get_optimal_permutation():
         a = b.copy()
         for j in xrange(len(a)):
             a[j] = perm_real[a[j]]
-        assert_array_equal(perm_real, get_optimal_permutation(a, b, k))
+        assert_array_equal(perm_real, _get_optimal_permutation(a, b, k))
     # case when permutation is possible but a and b have different labels
     a = np.array([1, 1, 2, 2, 0])
     b = np.array([3, 3, 4, 4, 0])
-    perm = get_optimal_permutation(a, b, k)
+    perm = _get_optimal_permutation(a, b, k)
     assert_array_equal(perm, np.array([0, 4, 3, 1, 2]))
 
     a = np.array([0, 0, 2, 2])
     b = np.array([3, 3, 1, 1])
-    perm = get_optimal_permutation(a, b, 4)
+    perm = _get_optimal_permutation(a, b, 4)
     assert_array_equal(perm, np.array([3, 2, 1, 0]))
 
     # case when we have different number of unique labels
@@ -66,59 +76,65 @@ def test_get_optimal_permutation():
     b = np.array([3, 3, 4, 4, 4])
     # in this case order matters
     # mapping a = p(b)
-    perm = get_optimal_permutation(a, b, 5)
+    perm = _get_optimal_permutation(a, b, 5)
     assert_array_equal(perm, np.array([4, 3, 0, 1, 2]))
     # mapping b = p(a)
-    perm = get_optimal_permutation(b, a, 5)
+    perm = _get_optimal_permutation(b, a, 5)
     assert_array_equal(perm, np.array([0, 3, 4, 2, 1]))
 
 
-def test_stability_score():
+def test_instability_score():
     a = np.arange(9)
     b = np.arange(9)
-    assert_equal(stability_score(a, b, 9), 0)
-    assert_equal(stability_score(a, b, 9), stability_score(b, a, 9))
-    assert_equal(stability_score(a, b[::-1], 9), 0)
+    assert_equal(_instability_score(a, b, 9), 0)
+    assert_equal(_instability_score(a, b, 9), _instability_score(b, a, 9))
+    assert_equal(_instability_score(a, b[::-1], 9), 0)
     for i in xrange(20):
         a = generate_random_labeling(5, 10)
         b = generate_random_labeling(5, 10)
-        score = stability_score(a, b, 10)
+        score = _instability_score(a, b, 10)
         assert_less_equal(score, 1.)
         assert_greater_equal(score, 0.)
         # test it's symmetric
-        assert_equal(score, stability_score(b, a, 10))
+        assert_equal(score, _instability_score(b, a, 10))
 
 
-def test_rand_stability_score():
-    for i in xrange(20):
-        k = np.random.randint(2, 10)
-        n = np.random.randint(k, 20)
-        s = np.random.randint(1, 20)
-        rand_score = rand_stability_score(k, n, s)
+def test_rand_instability_score():
+    s = 20
+    ks = np.arange(2, 21)
+    n = 30
+    rand_scores = rand_instability_score(ks, n, s)
+    assert_array_equal(rand_scores.keys(), ks)
+    for rand_score in rand_scores.values():
         assert_less_equal(rand_score, 1.)
         assert_greater_equal(rand_score, 0.)
 
 
-def test_norm_stability_score():
+def test_instability_class():
     a = np.arange(9)
     b = np.arange(9)
     s = 20
-    rand_score = rand_stability_score(len(np.unique(a)), len(a), s)
-    assert_equal(norm_stability_score(a, b, rand_score, 9), 0)
-    assert_equal(norm_stability_score(a, b[::-1], rand_score, 9), 0)
+    ks = np.arange(2, len(a)+1)
+    rand_scores = rand_instability_score(ks, len(a), s)
+    instability_score = InstabilityScore(rand_scores)
+    assert_equal(instability_score(a, b, 9), 0)
     for i in xrange(20):
         a = generate_random_labeling(5, 10)
         b = generate_random_labeling(5, 10)
-        rand_score = rand_stability_score(len(np.unique(a)), len(a), s)
-        score = norm_stability_score(a, b, rand_score, 10)
-        assert_greater_equal(score, 0.)
+        ks = np.arange(2, 11)
+        rand_scores = rand_instability_score(ks, len(a), s)
+        instability_score = InstabilityScore(rand_scores)
+        for k in ks:
+            score = instability_score(a, b, k)
+            assert_greater_equal(score, 0.)
 
 
-def test_correlation():
+def test_correlation_method():
+    cs = CorrelationScore()
     for i in xrange(10):
         x = np.random.normal(4, 2, size=10)
         y = np.random.normal(-3, 2, size=10)
-        assert_almost_equal(correlation(x, y), np.corrcoef(x, y)[0, 1])
+        assert_almost_equal(cs._correlation(x, y), np.corrcoef(x, y)[0, 1])
 
 
 def test_correlation_score():
@@ -126,6 +142,6 @@ def test_correlation_score():
     clust2 = np.random.normal(5, 1, size=(5, 10))
     true_clust = np.hstack((np.ones(10), np.zeros(10))).astype(int)
     d = np.hstack((clust1, clust2))
-    assert_equal(correlation_score(true_clust, true_clust, d), 1.0)
-    assert_equal(correlation_score(true_clust, true_clust, d,
-                                   corr_score='spearman'), 1.0)
+    for corr_type in ('spearman', 'pearson'):
+        correlation_score = CorrelationScore(corr_type=corr_type)
+        assert_equal(correlation_score(true_clust, true_clust, d), 1.0)
