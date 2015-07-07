f1d9a2da598545c2a61121577441e877a122a4c3
Matteo Visconti dOC <matteo.visconti.gr@dartmouth.edu>
2015-07-07 10:54:52 -0400

    TST: refactor tests with new class specification

--- reprclust/tests/test_cluster_methods.py
+++ reprclust/tests/test_cluster_methods.py
@@ -14,56 +14,26 @@ blob2 = 2*np.random.randn(10, 2) - 100
 data = np.vstack((blob1, blob2))
 
 def test_clustermethod():
-    assert_raises(ValueError, ClusterMethod, 30)
+    cm = ClusterMethod()
 
-    def my_cluster_method(x):
-        """just a fake"""
-        return 1
-
-    cm = ClusterMethod(my_cluster_method)
-
-    assert_raises(NotImplementedError, cm.cluster, 2)
+    assert_raises(NotImplementedError, cm.train, 2)
     assert_raises(NotImplementedError, cm.predict, 2)
-    assert_is_none(cm.get_clusters(2))
-    assert_is_none(cm.get_predicted_clusters(2))
-    assert_is_none(cm.data)
-    assert_false(cm.run)
-
-    cm(data)
-
-    assert_raises(NotImplementedError, cm.cluster, 2)
-    assert_raises(NotImplementedError, cm.predict, 2)
-    assert_is_none(cm.get_clusters(2))
-    assert_is_none(cm.get_predicted_clusters(2))
-    assert_false(cm.run)
-    assert_array_equal(cm.data, data)
+    assert_false(cm.is_trained)
 
 def _test_clustermethods(cm):
-    # store the data
-    cm(data)
-    # check we're not copying data
-    assert_is(data, cm.data)
     for k in range(2, 10):
-        assert_is_none(cm.cluster(k))
-        assert_is_none(cm.predict(data, k))
-        assert_array_equal(cm.get_clusters(k), cm.get_predicted_clusters(k))
-        # flip data and check that we actually get a different labeling
-        cm.predict(data[::-1], k)
-        assert_false(np.all(cm.get_clusters(k) ==
-                            cm.get_predicted_clusters(k)))
-        assert_array_equal(cm.get_clusters(k),
-                           cm.get_predicted_clusters(k)[::-1])
+        assert_is_none(cm.train(data, k))
+        assert_array_equal(cm.predict(data, k),
+                           cm.predict(data[::-1], k)[::-1])
         # XXX: temporary fix. apparently GMM might return less than the
         # number of components requested
-        if cm.method.__name__ is not 'GMM':
-            assert_equal(len(np.unique(cm.get_clusters(k))), k)
-
-    if hasattr(cm, 'get_method'):
-        for k in range(2, 10):
-            assert_is_not_none(cm.get_method(k))
+        if not isinstance(cm, GMMClusterMethod):
+            assert_equal(len(np.unique(cm.predict(data, k))), k)
 
 
 def test_implemented_clustermethods():
-    for cm in [WardClusterMethod, KMeansClusterMethod, GMMClusterMethod,
+    for cm in [WardClusterMethod,
+               KMeansClusterMethod,
+               GMMClusterMethod,
                CompleteClusterMethod]:
         yield _test_clustermethods, cm()
