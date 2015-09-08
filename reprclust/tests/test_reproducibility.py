cc669d3fd9827e7555fea3126a6eb3262fbcaccc
Matteo Visconti dOC <matteo.visconti.gr@dartmouth.edu>
2015-09-08 10:40:54 +0200

    TST: test raises

--- reprclust/tests/test_reproducibility.py
+++ reprclust/tests/test_reproducibility.py
@@ -2,10 +2,10 @@ from mvpa2.datasets.base import Dataset
 import numpy as np
 from numpy.testing import assert_array_equal
 
-from nose.tools import assert_is_none, assert_equal
+from nose.tools import assert_is_none, assert_equal, assert_raises
 
 from reprclust.cluster_methods import WardClusterMethod
-from reprclust.reproducibility import reproducibility
+from reprclust.reproducibility import reproducibility, _run_fold
 
 # create two far blobs easy to cluster
 blob1 = 2*np.random.randn(10, 2) + 100
@@ -35,3 +35,12 @@ def test_run_method():
         else:
             # only the first is 1.
             assert_equal(value[1, 0], 1.)
+
+def test_run_fold():
+    common_args = (fake_splitter[0][0], fake_splitter[0][1], WardClusterMethod, [2, 3])
+    # check raises
+    assert_raises(TypeError, _run_fold, blob1, *common_args)
+    assert_raises(ValueError, _run_fold, dss, *common_args, fa_space='subjects', sa_space='chunks')
+    assert_raises(KeyError, _run_fold, dss, *common_args, fa_space='runs')
+    assert_raises(KeyError, _run_fold, dss, *common_args, fa_space=None, sa_space='runs')
+
